bdfad81672fb16f8a38824c36794d516c3f2c1ba
CC <1522156171@qq.com>
2017-10-10 23:58:23 +0800

    update

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         /*testLog*/
-        Log.i("HelloLog","this is a test log");
+        Log.i("HelloLog","this is a second test log");
     }
 }
